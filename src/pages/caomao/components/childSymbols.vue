86a24290bae47c801cd674014ea941fc21b2df4e
qiufeihong <qiufeihong2018@126.com>
2019-01-07 18:03:55 +0800

    provide/inject

---
+++ src/pages/caomao/components/childSymbols.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>{{name}}</div>
+</template>
+
+<script>
+  export default {
+    name: 'ChildSymbols',
+    inject: {
+      name
+    }
+  }
+</script>
---
+++ src/pages/luFei/components/a.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    子组件:{{name}}
+    <button @click="handclick">是否响应</button>
+    <grand-children-inject></grand-children-inject>
+  </div>
+</template>
+
+<script>
+  import grandChildrenInject from './b'
+
+  export default {
+    name: 'childInject',
+    inject: ['name'],
+    components: {
+      grandChildrenInject
+    },
+    methods: {
+      handclick() {
+        this.name = 'author'
+      }
+    }
+  }
+</script>
