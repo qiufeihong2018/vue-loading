70523dc00e56b514e00e60270664715b5f374d35
qiufeihong <qiufeihong2018@126.com>
2019-03-05 10:14:03 +0800

    vue.observable取代vuex

--- src/pages/caomao/namei.vue
+++ src/pages/caomao/namei.vue
@@ -1,10 +1,27 @@
 <template>
-  <div>namei</div>
+  <section>
+    <article>
+      <div>在 Vue.js 2.6 中不使用 Vuex 来创建 store</div>
+      <p>count:{{count}}</p>
+      <button @click="setCount(count+1)">+1</button>
+      <button @click="setCount(count-1)">-1</button>
+    </article>
+  </section>
 </template>
 
 <script>
+  import { store, mutations } from '@/store/store'
+
   export default {
-    name: 'namei'
+    name: 'namei',
+    computed: {
+      count() {
+        return store.count
+      }
+    },
+    methods: {
+      setCount: mutations.setCount
+    }
   }
 </script>
 
