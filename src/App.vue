85aaa59dcf6bf04b2cc7d369c4e692a63dcc01c8
qiufeihong2018 <15058301288@163.com>
2019-11-16 22:48:53 +0800

    feat(增加上中下三栏布局)

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <script>
 export default {
   name: 'App'
-}
+};
 </script>
+<style lang="less">
+* {
+  margin: 0;
+  padding: 0;
+}
+</style>
--- src/pages/loading.vue
+++ src/pages/loading.vue
@@ -1,5 +1,41 @@
 <template>
-    <div>
-        fdsafasdf
-    </div>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-main>Main</el-main>
+    <el-footer>Footer</el-footer>
+  </el-container>
 </template>
+<script>
+export default {};
+</script>
+<style scoped lang="less">
+/* .el-header {
+}
+.el-main {
+}
+.el-footer {
+  height: 100px;
+  background-image: -webkit-linear-gradient(to right, #ff0000, #fff);
+} */
+
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+  width: 100%;
+}
+.el-header {
+}
+.el-footer {
+  bottom: 0;
+}
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  width: 100%;
+  height: calc(100vh - 60px);
+}
+</style>
