76afd9119c33f0527869dfd0e20ce1e3024c484d
qiufeihong2018 <15058301288@163.com>
2019-11-17 10:59:49 +0800

    feat(finish base layout)

--- src/pages/loading.vue
+++ src/pages/loading.vue
@@ -1,41 +1,150 @@
 <template>
   <el-container>
-    <el-header>Header</el-header>
-    <el-main>Main</el-main>
-    <el-footer>Footer</el-footer>
+    <el-header>
+      <span>vue-loading</span>
+    </el-header>
+    <el-main>
+      <el-row :gutter="40">
+        <el-col :span="6">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+        <el-col :span="6">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+        <el-col :span="6">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+        <el-col :span="6">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+      </el-row>
+      <el-row :gutter="40">
+        <el-col :span="6">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+        <el-col :span="6">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+        <el-col :span="6">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+        <el-col :span="6">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+      </el-row>
+      <el-row :gutter="40">
+        <el-col :span="6">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+        <el-col :span="6">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+        <el-col :span="6">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+        <el-col :span="6">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+      </el-row>
+      <el-row :gutter="40">
+        <el-col :span="6">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+        <el-col :span="6">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+        <el-col :span="6">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+        <el-col :span="6">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+      </el-row>
+      <el-row :gutter="40">
+        <el-col :span="6">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+        <el-col :span="6">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+        <el-col :span="6">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+        <el-col :span="6">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+      </el-row>
+      <el-row :gutter="40">
+        <el-col :span="6">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+        <el-col :span="6">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+        <el-col :span="6">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+        <el-col :span="6">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+      </el-row>
+    </el-main>
+    <el-footer>
+      <span>© 2019 www.qiufeihong.com MIT license</span>
+    </el-footer>
   </el-container>
 </template>
 <script>
 export default {};
 </script>
 <style scoped lang="less">
-/* .el-header {
-}
-.el-main {
-}
-.el-footer {
-  height: 100px;
-  background-image: -webkit-linear-gradient(to right, #ff0000, #fff);
-} */
-
 .el-header,
 .el-footer {
-  background-color: #b3c0d1;
-  color: #333;
-  text-align: center;
-  line-height: 60px;
+  padding: 22px 60px;
+  background-color: #393d49;
+  color: rgba(255, 255, 255, 0.8);
   width: 100%;
+  height: 100px !important;
 }
 .el-header {
+  font-size: 40px;
 }
 .el-footer {
   bottom: 0;
+  font-size: 15px;
+  text-align: center;
 }
 .el-main {
-  background-color: #e9eef3;
+  padding: 100px;
+  background-color: #eee;
   color: #333;
   text-align: center;
   width: 100%;
-  height: calc(100vh - 60px);
+  .el-row {
+    margin-bottom: 40px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #c2c2c2;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 395px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
 }
 </style>
